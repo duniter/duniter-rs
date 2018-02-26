72b13d593fe6caa16b91ebba5e7e0d63bd1a406f
nanocryk <nanocryk@ifee.fr>
2018-02-26 18:21:42 +0100

    [ref] new crate duniter-crypto replacing  duniter-keys

---
+++ crypto/README.md
@@ -0,0 +1,7 @@
+# crypto
+
+`duniter-crypto` is a crate managing cryptographic building blocks for the Duniter project.
+
+## How to use it
+
+You can add `duniter-crypto` as a `cargo` dependency in your Rust project.
---
+++ crypto/lib.rs
@@ -0,0 +1,26 @@
+//  Copyright (C) 2018  The Duniter Project Developers.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Provide wrappers for cryptographic building blocks used by Duniter.
+
+#![deny(missing_docs, missing_debug_implementations, missing_copy_implementations, trivial_casts,
+        trivial_numeric_casts, unsafe_code, unstable_features, unused_import_braces,
+        unused_qualifications)]
+
+extern crate base58;
+extern crate base64;
+extern crate crypto;
+
+pub mod keys;
