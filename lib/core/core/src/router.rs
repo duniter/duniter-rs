duniter/duniter-rs

commit 1fc6292cb28a70c7b5b382dee9d92d8599acf537

[fix] core: #134 remove unwrap

lib/core/core/src/router.rs
@@ -157,20 +157,20 @@ fn start_broadcasting_thread(
                         local_node_endpoints.append(&mut module_endpoints);
 
                         // If all modules registered
-                        if expected_registrations_count.is_some()
-                            && registrations_count == expected_registrations_count.unwrap()
-                        {
-                            // Get list of InterNodesNetwork modules
-                            let receivers = roles
-                                .get(&ModuleRole::InterNodesNetwork)
-                                .expect("Fatal error : no module with role InterNodesNetwork !")
-                                .to_vec();
-                            // Send endpoints to network module
-                            send_msg_to_several_receivers(
-                                DursMsg::ModulesEndpoints(local_node_endpoints.clone()),
-                                &receivers,
-                                &modules_senders,
-                            );
+                        if let Some(expected_regs_count) = expected_registrations_count {
+                            if registrations_count == expected_regs_count {
+                                // Get list of InterNodesNetwork modules
+                                let receivers = roles
+                                    .get(&ModuleRole::InterNodesNetwork)
+                                    .expect("Fatal error : no module with role InterNodesNetwork !")
+                                    .to_vec();
+                                // Send endpoints to network module
+                                send_msg_to_several_receivers(
+                                    DursMsg::ModulesEndpoints(local_node_endpoints.clone()),
+                                    &receivers,
+                                    &modules_senders,
+                                );
+                            }
                         }
                         // Add this sender to modules_senders
                         modules_senders.insert(module_static_name, module_sender);
@@ -215,18 +215,24 @@ fn start_broadcasting_thread(
                 RecvTimeoutError::Disconnected => fatal_error!("router thread disconnnected !"),
             },
         }
-        if (expected_registrations_count.is_none()
-            || registrations_count < expected_registrations_count.unwrap())
-            && SystemTime::now()
-                .duration_since(start_time)
-                .expect("Duration error !")
-                .as_secs()
-                > *MAX_REGISTRATION_DELAY
-        {
+        if let Some(expected_regs_count) = expected_registrations_count {
+            if registrations_count < expected_regs_count
+                && SystemTime::now()
+                    .duration_since(start_time)
+                    .expect("Duration error !")
+                    .as_secs()
+                    > *MAX_REGISTRATION_DELAY
+            {
+                fatal_error!(
+                    "{} modules have registered, but expected {} !",
+                    registrations_count,
+                    expected_regs_count
+                );
+            }
+        } else {
             fatal_error!(
-                "{} modules have registered, but expected {} !",
-                registrations_count,
-                expected_registrations_count.unwrap_or(0)
+                "{} modules have registered, but none expected !",
+                registrations_count
             );
         }
     }

lib/core/core/src/commands/keys.rs
@@ -143,7 +143,7 @@ impl DursExecutableCoreCommand for KeysOpt {
 
         match self.subcommand {
             KeysSubCommand::Wizard(_) => {
-                let new_keypairs = key_wizard(keypairs).unwrap();
+                let new_keypairs = key_wizard(keypairs)?;
                 save_keypairs(profile_path, &keypairs_file, new_keypairs)
                     .map_err(DursCoreError::FailWriteKeypairsFile)
             }

lib/core/core/src/errors.rs
@@ -17,6 +17,7 @@
 
 use crate::logger::InitLoggerError;
 use dubp_currency_params::db::CurrencyParamsDbError;
+use durs_conf::keys::WizardError;
 use durs_module::{ModuleStaticName, PlugModuleError};
 use failure::{Error, Fail};
 
@@ -64,10 +65,19 @@ pub enum DursCoreError {
     /// Sync without source and without option local
     #[fail(display = "Please specify the url of a trusted node or use the --local option.")]
     SyncWithoutSource,
+    /// Error on keys sub-command
+    #[fail(display = "Error en keys sub-command")]
+    WizardKeysError(WizardError),
 }
 
 impl From<InitLoggerError> for DursCoreError {
     fn from(e: InitLoggerError) -> Self {
         DursCoreError::InitLoggerError(e)
+    }
+}
+
+impl From<WizardError> for DursCoreError {
+    fn from(e: WizardError) -> Self {
+        DursCoreError::WizardKeysError(e)
     }
 }

commit 1f7d02ca0ef4c65236190ccda34d804977c43d1d

[fix] core: revert regression introduce by commit 1fc6292c

lib/core/core/src/router.rs
@@ -215,24 +215,18 @@ fn start_broadcasting_thread(
                 RecvTimeoutError::Disconnected => fatal_error!("router thread disconnnected !"),
             },
         }
-        if let Some(expected_regs_count) = expected_registrations_count {
-            if registrations_count < expected_regs_count
-                && SystemTime::now()
-                    .duration_since(start_time)
-                    .expect("Duration error !")
-                    .as_secs()
-                    > *MAX_REGISTRATION_DELAY
-            {
-                fatal_error!(
-                    "{} modules have registered, but expected {} !",
-                    registrations_count,
-                    expected_regs_count
-                );
-            }
-        } else {
+        if (expected_registrations_count.is_none()
+            || registrations_count < unwrap::unwrap!(expected_registrations_count))
+            && SystemTime::now()
+                .duration_since(start_time)
+                .expect("Duration error !")
+                .as_secs()
+                > *MAX_REGISTRATION_DELAY
+        {
             fatal_error!(
-                "{} modules have registered, but none expected !",
-                registrations_count
+                "{} modules have registered, but expected {} !",
+                registrations_count,
+                expected_registrations_count.unwrap_or(0)
             );
         }
     }